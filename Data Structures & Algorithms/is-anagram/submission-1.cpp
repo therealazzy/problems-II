e51fd31ae444860a7b4f3d81cdbc16b7fdbdb8e8
therealazzy <71785801+therealazzy@users.noreply.github.com>
2026-04-06 11:53:13 +0100

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char, int> sCount;
+        unordered_map<char, int> tCount;
+        for(int i = 0; i < s.size(); ++i){
+            sCount[s[i]]++;
+            tCount[t[i]]++;
+        }
+
+        return sCount == tCount;
+    }
+};
