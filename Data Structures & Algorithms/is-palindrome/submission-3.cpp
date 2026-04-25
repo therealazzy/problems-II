19ebd28c9e5729d4e54fc063904e17890de7deee
therealazzy <71785801+therealazzy@users.noreply.github.com>
2026-04-25 14:15:48 +0100

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        //two pointer method
+            int l = 0;
+            int r = s.size()-1;
+            while(l < r){
+            while(l<r && !isalnum(s[l])) l++;
+            while(l < r && !isalnum(s[r])) r--;
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            l++;
+            r--;
+            }
+
+        return true;
+    }
+};
