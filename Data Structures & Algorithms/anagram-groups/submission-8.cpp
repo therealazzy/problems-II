8a77d072120db80e923d88996d74c98b3e7c9fbe
therealazzy <71785801+therealazzy@users.noreply.github.com>
2026-05-01 12:54:38 +0100

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<string>> group;
+        vector<vector<string>> res;
+        
+        for(string& s : strs){
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            group[sorted].push_back(s);
+        }
+        for(auto& g : group){
+            res.push_back(g.second);
+        }
+        return res;
+    }
+};
