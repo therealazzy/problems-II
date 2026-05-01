49eb66a985c23bc909a9daa7de53186d63cdbc52
therealazzy <71785801+therealazzy@users.noreply.github.com>
2026-05-01 14:50:22 +0100

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size()-1;
+        while(l < r){
+            while(l<r && !isval(s[l])) l++;
+            while(l < r && !isval(s[r])) r--;
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    //optional custom implementation of isalnum function
+    bool isval(char c) {
+        unsigned char uc = static_cast<unsigned char>(c);
+        return (uc >= '0' && uc <= '9') || (uc >= 'A' && uc <= 'Z') || (uc >= 'a' && uc <= 'z');
+    }
+
+};
